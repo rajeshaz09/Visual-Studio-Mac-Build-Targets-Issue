ec4c218905c4157e75018e42cdf2fe6c1ef7ecc4
Rajesh Aravapalli <rajeshaz09@outlook.com>
2023-04-24 09:59:02 +0530

    Initial version

---
+++ Program.cs
@@ -0,0 +1,90 @@
+using Microsoft.Data.Sqlite;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+PerformDbOperations();
+
+app.MapGet("/", () => new
+{
+    Id = Guid.NewGuid(),
+    Message = "Hello World!"
+});
+
+app.Run();
+
+
+void PerformDbOperations()
+{
+    using (var connection = new SqliteConnection("Data Source=hello.db"))
+    {
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText =
+        @"
+                    CREATE TABLE user (
+                        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                        name TEXT NOT NULL
+                    );
+                    INSERT INTO user
+                    VALUES (1, 'Brice'),
+                           (2, 'Alexander'),
+                           (3, 'Nate');
+                ";
+        command.ExecuteNonQuery();
+
+        Console.Write("Name: ");
+        var name = Console.ReadLine();
+
+        #region snippet_Parameter
+        command.CommandText =
+        @"
+                    INSERT INTO user (name)
+                    VALUES ($name)
+                ";
+        command.Parameters.AddWithValue("$name", name);
+        #endregion
+        command.ExecuteNonQuery();
+
+        command.CommandText =
+        @"
+                    SELECT last_insert_rowid()
+                ";
+        var newId = (long)command.ExecuteScalar();
+
+        Console.WriteLine($"Your new user ID is {newId}.");
+    }
+
+    Console.Write("User ID: ");
+    var id = int.Parse(Console.ReadLine());
+
+    #region snippet_HelloWorld
+    using (var connection = new SqliteConnection("Data Source=hello.db"))
+    {
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText =
+        @"
+                    SELECT name
+                    FROM user
+                    WHERE id = $id
+                ";
+        command.Parameters.AddWithValue("$id", id);
+
+        using (var reader = command.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                var name = reader.GetString(0);
+
+                Console.WriteLine($"Hello, {name}!");
+            }
+        }
+    }
+    #endregion
+
+    // Clean up
+    File.Delete("hello.db");
+}
+}
